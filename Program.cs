5e6a6b17fca480c84cc281e041e916647dcdd9ec
Sherzodbek Sharipov <sherzodbekdeveloper@gmail.com>
2024-01-13 18:51:21 +0500

    In Progess

--- Program.cs
+++ Program.cs
@@ -1,44 +1,57 @@
 ﻿using System;
 
- Console.WriteLine("Welcome to Calculator programm!");
- Console.WriteLine(" Enter the first number...");
+Console.WriteLine("Welcome to Calculator programm!");
+Console.WriteLine(" Enter the first number...");
 int firstValue;
-    if (int.TryParse(Console.ReadLine(), out firstValue))
-    {
-    }
-    else
-    {
-        Console.WriteLine("Invalid format! Please enter a valid integer.");
-        return;
-    }
+if (int.TryParse(Console.ReadLine(), out firstValue))
+{
+}
+else
+{
+    Console.WriteLine("Invalid format! Please enter a valid integer.");
+    return;
+}
 
 Console.WriteLine("Enter second Number");
 int secondValue;
-    if (int.TryParse(Console.ReadLine(), out secondValue))
-    {
-    }
-    else
-    {
-        Console.WriteLine("Invalid format! Please enter a valid integer.");
-        return;
-    }
+if (int.TryParse(Console.ReadLine(), out secondValue))
+{
+}
+else
+{
+    Console.WriteLine("Invalid format! Please enter a valid integer.");
+    return;
+}
 
 if (firstValue > secondValue)
-    {
-        Console.WriteLine("First Number is greater !");
-    }
+{
+    Console.WriteLine("First Number is greater !");
+}
 else if (firstValue == secondValue)
-    {
-        Console.WriteLine("Numbers are equal !");
-    }
+{
+    Console.WriteLine("Numbers are equal !");
+}
 else
-    {
-        Console.WriteLine("Second Number is greater !");
-    }
-
+{
+    Console.WriteLine("Second Number is greater !");
+}
+// logical operations in ternary 
+// logical operators   &&   ||   !(bla bla bla)
 string message =
-    firstValue > 0
-    ? "First Value is positive !"
-    : "First Value is negative";
+    firstValue >= 0 && secondValue >= 0
+    ? "First and second Numbers are positive !"
+    : "First and second Number are negative";
 Console.WriteLine(message);
+
+string messag =
+    firstValue >= 0 || secondValue >= 0
+    ? "First or second Number is positive !"
+    : "First and second Numbers are negative";
+Console.WriteLine(messag);
+
+string messa =
+    !(firstValue >= 0)
+    ? "First Number is Negative !"
+    : "First Number is Positive";
+Console.WriteLine(messa);
 
